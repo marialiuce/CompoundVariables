97b8206cb39d628adc28afa3303f2c9a68e5a512
Alice <malyefx@gmail.com>
2025-05-21 12:04:49 -0300

    segundo commit

---
+++ alunos.js
@@ -0,0 +1,4 @@
+let nomes = ['Lucas', 'Ana', 'Pedro', 'Maria', 'João']
+for (let i = 0; i < nomes.length; i++) {
+    console.log(`Olá, ${nomes[i]}!`)
+}
---
+++ listadecompras.js
@@ -0,0 +1,4 @@
+let listaDeCompras = ['Arroz', 'Feijão', 'Macarrão', 'Carne', 'Frango']
+console.log(listaDeCompras[3])
+listaDeCompras[0] = 'Batata' // Troca o valor do índice 0
+console.log(listaDeCompras[0])
